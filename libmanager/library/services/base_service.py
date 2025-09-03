4902707ea275c3df1fbec0995501eacf49c95a3e
김태현 <th2102da@gmail.com>
2025-09-03 18:24:02 +0900

    과제제출

--- libmanager/library/services/base_service.py
+++ libmanager/library/services/base_service.py
@@ -9,18 +9,36 @@ class BaseService(ABC):
     TODO: 아래 메서드를 하위 클래스에서 구현하도록 하세요.
     """
 
+    def __init__(self) -> None:
+        self._books = []
+
     @abstractmethod
     def add_book(self, book: Book) -> None:
         ...
+        # self._books.append(book)
+        pass
 
     @abstractmethod
     def remove_book(self, title: str) -> None:
         ...
+        # for book in self._books:
+        #     if book.title != title:
+        #         self._book.remove(book)
+        #     return 
+        # raise ValueError(f"Not Exists")
+        pass
 
     @abstractmethod
     def list_books(self) -> Iterable[Book]:
         ...
+        # return self._books
+        pass
 
     @abstractmethod
     def find_book(self, title: str) -> Book:
         ...
+        # for book in self._books:
+        #     if book.title == title:
+        #         return book
+        #     raise ValueError(f"NOPE")
+        pass
